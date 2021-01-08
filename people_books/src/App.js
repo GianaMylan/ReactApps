f6295a31dc565e2626d204de1cb41a3abad6ebbb
Giana Mylan <Giana Mylan>
2021-01-08 15:39:41 -0500

    first commit

---
+++ people_books/src/App.js
@@ -0,0 +1,261 @@
+import React from 'react';
+import './App.css';
+import axios from 'axios';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+const PEOPLE_URL = 'http://myapi-profstream.herokuapp.com/api/5eaf5c/persons'
+const BOOKS_URL = 'http://myapi-profstream.herokuapp.com/api/e8f2c9/books'
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/persons">People</Link>
+          </li>
+          <li>
+            <Link to="/books">Books</Link>
+          </li>
+        </ul>
+        <Switch>
+          <Route path="/persons">
+            <Persons />
+          </Route>
+          <Route path="/books">
+            <Books />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+    );
+  }
+}
+
+class Home extends React.Component {
+  render() {
+    return (
+      <div className="home">
+      <h1> Home component works! </h1>
+      </div>
+    )
+  }
+}
+
+class Persons extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleDelete = this.handleDelete.bind(this);
+  }
+
+  async getPersons() {
+    try {
+      const res = await axios.get(PEOPLE_URL) ; 
+      this.setState({ persons: res.data });
+    } catch(e) {
+      console.error(e.message); 
+    }
+  }
+
+  componentDidMount() {
+    this.getPersons();
+  }
+
+  handleChange(e) {
+    const {name, value } = e.target;
+    this.setState({ [name]: value })
+  }
+
+  async handleSubmit(e) {
+    e.preventDefault();
+    const { firstname, lastname, email, username } = this.state;
+    const persons = { firstname, lastname, email, username };
+    try {
+      const res = await axios.post(PEOPLE_URL, persons);
+      console.log(res.data);
+      const updateRes = await axios.get(PEOPLE_URL);
+      this.setState ({ persons: updateRes.data });
+    } catch(e) {
+      console.error(e.message)
+    }
+  }
+
+  async handleDelete(e) {
+    const { id } = e.target;
+    try {
+      const res = await axios.delete(PEOPLE_URL + id);
+      console.log(res.data);
+      const updateRes = await axios.get(PEOPLE_URL);
+      this.setState({ people: updateRes.data });
+    } catch(e) {
+      console.error(e.message)
+    }
+  }
+
+  render() {
+    return (
+      <div className="persons">
+        <h1>Title for Persons!</h1>
+        <ul>
+          <div className="parent"> 
+            { 
+            this.state.persons && this.state.persons.map(persons => (
+              <li className="children">
+              Name: { persons.firstname } { persons.lastname } 
+              <br></br>
+              Email: { persons.email }
+              <br></br>
+              Username: { persons.username }
+              <br></br> <br></br>
+              <button className="deleteButton" onClick={ () => this.handleDelete(persons.id)}>Delete User</button><br></br> 
+              </li> 
+            ))
+            }
+          </div> <br></br>
+          <br></br>
+        </ul> <br></br>
+        <div>
+          <form className="newPersonForm"
+            onChange={ this.handleChange }
+            onSubmit={ this.handleSubmit }>
+            <h3> Add a new user:</h3>
+            <label>
+              First Name: 
+              <input type="text" name="firstname" />
+            </label> <br></br> <br></br>
+            <label>
+              Last Name: 
+              <input type="text" name="lastname" />
+            </label> <br></br> <br></br>
+            <label>
+              Email: 
+              <input type="text" name="email" />
+            </label> <br></br> <br></br>
+            <label>
+              Username: 
+              <input type="text" name="username" />
+            </label> <br></br> <br></br> 
+            <input type="submit" className="submitButton" /> 
+          </form>
+        </div>
+      </div>
+    )
+  }
+}
+//firstname, lastname, email, username
+
+
+class Books extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  async getBooks () {
+    try {
+      const res = await axios.get(BOOKS_URL);
+      this.setState({ books: res.data });
+    } catch(e) {
+      console.error(e.message);
+    }
+  }
+
+  componentDidMount() {
+    this.getBooks();
+  }
+
+  async handleChange(e) {
+    const {name, value } = e.target;
+    this.setState({ [name]: value })
+  }
+
+  async handleSubmit(e) {
+    e.preventDefault();
+    const { title, author, release_data, image } = this.state;
+    const books = { title, author, release_data, image };
+    try {
+      const res = await axios.post(BOOKS_URL, books);
+      console.log(res.data);
+      const updateRes = await axios.get(BOOKS_URL);
+      this.setState ({ books: updateRes.data });
+    } catch(e) {
+      console.error(e.message)
+    }
+  }
+// title, author, release_date, image
+  async handleDelete(e) {
+    const { id } = e.target;
+    try {
+      const res = await axios.delete(BOOKS_URL + id);
+      console.log(res.data);
+      const updateRes = await axios.get(BOOKS_URL);
+      this.setState({ books: updateRes.data });
+    } catch(e) {
+      console.error(e.message)
+    }
+  }
+
+  render () {
+    return (
+      <div className="books">
+        <h1>Books component Working!</h1>
+        <div>
+          <div className="parent">
+          <ul>
+            {
+              this.state.books && this.state.books.map(books =>
+                <li className="children">
+                  Title: { books.title } <br></br>
+                  Author: { books.author } <br></br>
+                  Released on { books.release_date } <br></br>
+                  <br></br> 
+                  <button className="deleteButton" onClick={ () => this.handleDelete(books.id)}>Delete Entry </button><br></br>
+                </li>)
+            }
+          </ul> <br></br>
+          </div>
+          <div>
+            <form className="bookForm"
+            onChange = { this.handleChange }
+            onSubmit = { this.handleSubmit }>
+              <h3> Add a new book: </h3>
+              <label>
+                Title: 
+                <input type="text" name="title" />
+              </label> <br></br> <br></br>
+              <label>
+                Author: 
+                <input type="text" name="author" />
+              </label> <br></br> <br></br>
+              <label>
+                Released on: 
+                <input type="text" name="released" />
+              </label> <br></br> <br></br>
+              <button className="submitButton" >Add New Book</button>
+            </form>
+          </div> {/* form div */}
+        </div> {/*booksContainer div */}
+      </div>
+    )
+  }
+}
+// title, author, release_date, image
+
+export default App;
---
+++ wines_app/src/App.js
@@ -0,0 +1,241 @@
+// installed axios - npm i axios. What does axios do? axios make HTTP requests (calls)
+  // import axios
+// installed react router - npm i react-router-dom. What does router do? Router generates urls
+  // import react-router-dom
+  import React from 'react';
+  import axios from 'axios';
+  import './App.css';
+  import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+  } from "react-router-dom";
+  const WINES_URL = 'http://myapi-profstream.herokuapp.com/api/7fe3ba/wines'
+  class App extends React.Component {
+    render() {
+      return (
+        <Router>
+          <div>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/wines">Wines</Link>
+              </li>
+            </ul>
+            <Switch>
+              <Route path="/wines">
+                <Wines />
+              </Route>
+              <Route path="/">
+                <Home />
+              </Route>
+            </Switch>
+          </div>
+        </Router>
+      );
+    }
+  }
+  class Home extends React.Component {
+    render() {
+      return(
+        <h1>Home component works!</h1>
+      )
+    }
+  }
+  class Wines extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {}
+      this.handleChange = this.handleChange.bind(this);
+      this.handleSubmit = this.handleSubmit.bind(this);
+      this.selectWine = this.selectWine.bind(this);
+      this.editWine = this.editWine.bind(this);
+      this.submitEditedWine = this.submitEditedWine.bind(this);
+    }
+    async getWines() {
+      try {
+        const res = await axios.get(WINES_URL);
+        this.setState({ wines: res.data });
+      } catch(e) {
+        console.error(e);
+      }
+    }
+
+    componentDidMount() {
+      this.getWines();
+    }
+
+    handleChange(e) {
+      const { name, value } = e.target;
+      // ^^ assigns name and value to e.target as a general
+      this.setState({ [name]: value })
+    }
+
+    async handleSubmit(e) {
+      e.preventDefault();
+      const { name, year, grapes, country, region, description, picture, price } = this.state; 
+      //this is so you dont need to this.state.'something' for each
+      //console.log({ name, year, grapes, country, region, description, picture, price })
+      const wine = { name, year, grapes, country, region, description, picture, price };
+      try {
+        const res = await axios.post(WINES_URL, wine);
+        console.log(res.data);
+        const updateRes = await axios.get(WINES_URL);
+        this.setState({ wines: updateRes.data });
+      } catch(e) {
+        console.error(e.message);
+      }
+    }
+
+    async handleDelete(id) {
+      console.log(WINES_URL + id);
+      //const { id } = ev.target;
+      try {
+        const res = await axios.delete(WINES_URL + id); // target wine id
+        console.log(res.data);
+        const updateRes = await axios.get(WINES_URL);
+        this.setState({ wines: updateRes.data});
+      } catch(e) {
+        console.error(e.message)
+      }
+    }
+
+    selectWine(selectedWine) {
+      //put wine in selected state
+      this.setState({ selectedWine });
+
+      //input fields to edit
+      //we may want to use ID
+      //axios.patch
+      //console.log(selectedWine);
+    }
+
+    editWine(e){
+      //console.log(e.target.value)
+      const { name, value } = e.target
+      this.setState({...this.state, selectedWine: { ...this.state.selectedWine, [name]: value }})
+    }
+
+    async submitEditedWine(e) {
+      e.preventDefault();
+      try {
+        const editedWine = this.state.selectedWine; // this obj has an id
+        console.log(editedWine)
+        // to send our patch to url + /:id
+        const focusWine = WINES_URL + editedWine.id
+        const res = await axios.patch(focusWine, editedWine);
+        const resRefresh = await axios.get(WINES_URL);
+        this.setState({ wines: resRefresh.data });
+      } catch(e) {
+        console.error(e);
+      }
+    }
+
+    render() {
+      return (
+        <div className="wines">
+          <ul>
+            {/* render info */}
+            {
+              this.state.wines && this.state.wines.map(wine => (
+              <li key={ wine.id }>
+                { wine.name }:  ${ wine.price } 
+                <button onClick={ () => this.handleDelete(wine.id) }>Delete Wine</button>
+                <button onClick={() => this.selectWine(wine)}>Edit Wine</button>
+              </li>
+              ))
+            }
+          </ul>
+          <form className="new-wine-form"
+            onChange={ this.handleChange }
+            onSubmit={ this.handleSubmit }>
+              <h3> Add a New Wine: </h3>
+            <label>
+              Wine name:
+              <input type="text" name="name"/>
+            </label>
+            <label>
+              Year wine was made:
+              <input type="text" name="year"/>
+            </label>
+            <label>
+              Grapes used:
+              <input type="text" name="grapes" />
+            </label>
+            <label>
+              Country of wine:
+              <input type="text" name="country" />
+            </label>
+            <label>
+              Wine region:
+              <input type="text" name="region" />
+            </label>
+            <label>
+              Description of wine:
+              <input type="text" name="description" />
+            </label>
+            <label>
+              Picture url:
+              <input type="text" name="picture" />
+            </label>
+            <label>
+              Price:
+              <input type="text" name="price" />
+            </label>
+            <input type="submit" className="submitButton" />
+          </form>
+          
+          <hr></hr>
+          {/* we want form to show */}
+          {/* only after we click on it */}
+          {/* if this.stat.selectedWine exists */}
+          {/* this.state.selectedWine && formBelow */}
+          
+          { this.state.selectedWine && 
+          <form className="wine-edit-form"
+          onChange ={ this.editWine }>
+            <h3>Edit Wine: </h3>
+            <label>
+              Wine name:
+              <input type="text" name="name" defaultValue={ this.state.selectedWine.name } />
+            </label>
+            <label>
+              Year wine was made:
+              <input type="text" name="year" defaultValue={ this.state.selectedWine.year } />
+            </label>
+            <label>
+              Grapes used:
+              <input type="text" name="grapes" defaultValue={ this.state.selectedWine.grapes } />
+            </label>
+            <label>
+              Country of wine:
+              <input type="text" name="country" defaultValue={ this.state.selectedWine.country } />
+            </label>
+            <label>
+              Wine region:
+              <input type="text" name="region" defaultValue={ this.state.selectedWine.region } />
+            </label>
+            <label>
+              Description of wine:
+              <input type="text" name="description" defaultValue={ this.state.selectedWine.description } />
+            </label>
+            <label>
+              Picture url:
+              <input type="text" name="picture" defaultValue={ this.state.selectedWine.picture } />
+            </label>
+            <label>
+              Price:
+              <input type="text" name="price" defaultValue={ this.state.selectedWine.price } />
+            </label>
+            <input type="submit" className="submitButton" />
+          </form> }
+        </div>
+      )
+    }
+  }
+
+  
+  export default App;
